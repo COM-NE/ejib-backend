f7a381bb8701dfb80d0ea4f85113f44c229ee898
TaeHoon <taehoondev030@gamil.com>
2026-03-30 20:53:39 +0900

    initial commit

---
+++ src/main/java/com/comne/ejib/EjibApplication.java
@@ -0,0 +1,13 @@
+package com.comne.ejib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comne/ejib/EjibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comne.ejib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
