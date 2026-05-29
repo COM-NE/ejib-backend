75c110df6272ad2b8dbd899913ef752ba40e6e42
seoyeoneel02 <seoyeoneel02@gmail.com>
2026-05-29 10:43:14 +0900

    fix: test profile 적용

--- src/test/java/com/comne/ejib/EjibApplicationTests.java
+++ src/test/java/com/comne/ejib/EjibApplicationTests.java
@@ -2,8 +2,10 @@ package com.comne.ejib;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EjibApplicationTests {
 
 	@Test
